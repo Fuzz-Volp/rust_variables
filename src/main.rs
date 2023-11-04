0657db7ce62d4b2cc64b6e842dbdac2d122f52c3
John Volpitta <johnvolpitta@gmail.com>
2023-11-04 09:10:56 -0700

    finishing class on variables.

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+fn main() {
+    let name = "Fuzzy";
+    let mut age = 27;
+    println!("{}", age);
+    
+    let amount:i64 = 8473927858472; 
+    
+    age = 43;
+    
+    println!("{}", age);
+
+    let color = "blue";
+    
+    let color = 86;
+    
+    println! ("{}", color);
+
+
+    let (a, b, c,) = (43, 85, "red");
+
+}
